3fbc8f6b2f0b39ca7aeaa542d33d8d85eb64b23a
lIlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl <personalprotonmailbox@proton.me>
2025-02-03 11:36:45 +0100

    BUGFIX - Activation de l'accès à l'image de la carte du monde en local (avec Live Server)

--- src/scripts/generation/modern-design/globe/componentGeneration.js
+++ src/scripts/generation/modern-design/globe/componentGeneration.js
@@ -4,11 +4,10 @@ class EarthGlobeComponent extends HTMLElement {
             <section>
                 <div class="container">
                     <h2>Lorem ipsum</h2>
-                        <canvas id="earthCanvas"></canvas>
-                        <svg id="pointsSVG" class="points-overlay"></svg>
+                    <canvas id="earthCanvas"></canvas>
+                    <svg id="pointsSVG" class="points-overlay"></svg>
                     <img id="mapImage"
-                        src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Carte_du_monde_crete.png"
-                        crossorigin="anonymous">
+                        src="src/assets/images/illustrations/map.png" crossOrigin="anonymous">
                 </div>
             </section>
         `;
